47d208bce3db1e75fa037aaf5375df17013f4b23
shahnmah <shahn.mah@gmail.com>
2022-05-17 21:50:33 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
